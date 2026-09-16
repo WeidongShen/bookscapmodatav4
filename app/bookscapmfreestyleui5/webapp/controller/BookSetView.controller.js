b4a12e00cb3af4f17dabc38596cf2318f783072d
Weidong Shen <weidongshen@gmail.com>
2026-09-16 21:04:02 +0000

    Added event handler for show chapters

--- app/bookscapmfreestyleui5/webapp/controller/BookSetView.controller.js
+++ app/bookscapmfreestyleui5/webapp/controller/BookSetView.controller.js
@@ -18,6 +18,22 @@ sap.ui.define([
             // Sorters
             var oSorter = new sap.ui.model.Sorter('price', true);
             oBinding.sort(oSorter);
+        },
+        // Event handler for show chapters
+        async _showChapters(oEvent) {
+            var oModel = this.getView().getModel('book');
+            var oBookContext = oEvent.getParameter('listItem').getBindingContext('book');
+
+            if (!oBookContext) return;
+            var sId = oBookContext.getProperty('ID');
+            var sPath = `/BooksSet(ID=${sId})`;
+            // bind context with expand
+            var oCtxBinding = oModel.bindContext(sPath, null, {$expand : 'chapters'});
+
+            var oObj =await oCtxBinding.requestObject();
+            var aChapters = (oObj && oObj.chapters) ? oObj.chapters : [];
+
+            this.getView().setModel(new sap.ui.model.json.JSONModel(aChapters), 'ch');
         }
     });
 });
