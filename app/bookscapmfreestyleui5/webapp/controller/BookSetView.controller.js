0f264167101e23227486f55e9bd3856b3d424f1a
Weidong Shen <weidongshen@gmail.com>
2026-09-16 19:43:23 +0000

    Added filter and sorter

--- app/bookscapmfreestyleui5/webapp/controller/BookSetView.controller.js
+++ app/bookscapmfreestyleui5/webapp/controller/BookSetView.controller.js
@@ -5,6 +5,19 @@ sap.ui.define([
 
     return Controller.extend("bookscapmfreestyleui5.controller.BookSetView", {
         onInit() {
+            var oDataModel = this.getOwnerComponent().getModel();    // OData V4
+            this.getView().setModel(oDataModel, 'book');
+
+            // Filters
+            var oTable = this.getView().byId('idBooksTable');
+            var oBinding = oTable.getBinding('items');
+            var oFilters = [
+                new sap.ui.model.Filter('price', 'LT', 10)
+            ];
+            oBinding.filter(oFilters);
+            // Sorters
+            var oSorter = new sap.ui.model.Sorter('price', true);
+            oBinding.sort(oSorter);
         }
     });
 });
